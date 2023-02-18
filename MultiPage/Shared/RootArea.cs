df274aec6cac561f2e4f0478d7c5ddf43286e600
Jimbos <tr9l9l9@hotmail.com>
2023-02-19 00:13:17 +0200

    Changed project to blazor web assembly and added navigation to Pokemon page.

---
+++ MultiPage/Shared/RootArea.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiPage.Shared {
+    using System.Text.Json.Serialization;
+
+    namespace StockTracker.Shared {
+        // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+        public class RootArea {
+            [JsonPropertyName("areas")]
+            public List<Area> areas { get; set; }
+
+            [JsonPropertyName("game_indices")]
+            public List<GameIndex> game_indices { get; set; }
+
+            [JsonPropertyName("id")]
+            public int id { get; set; }
+
+            [JsonPropertyName("name")]
+            public string name { get; set; }
+
+            [JsonPropertyName("names")]
+            public List<Name> names { get; set; }
+
+            [JsonPropertyName("region")]
+            public Region region { get; set; }
+        }
+
+        public class Area {
+            [JsonPropertyName("name")]
+            public string name { get; set; }
+
+            [JsonPropertyName("url")]
+            public string url { get; set; }
+        }
+
+        public class GameIndex {
+            [JsonPropertyName("game_index")]
+            public int game_index { get; set; }
+
+            [JsonPropertyName("generation")]
+            public Generation generation { get; set; }
+        }
+
+        public class Generation {
+            [JsonPropertyName("name")]
+            public string name { get; set; }
+
+            [JsonPropertyName("url")]
+            public string url { get; set; }
+        }
+
+        public class Language {
+            [JsonPropertyName("name")]
+            public string name { get; set; }
+
+            [JsonPropertyName("url")]
+            public string url { get; set; }
+        }
+
+        public class Name {
+            [JsonPropertyName("language")]
+            public Language language { get; set; }
+
+            [JsonPropertyName("name")]
+            public string name { get; set; }
+        }
+
+        public class Region {
+            [JsonPropertyName("name")]
+            public string name { get; set; }
+
+            [JsonPropertyName("url")]
+            public string url { get; set; }
+        }
+
+    }
+}
