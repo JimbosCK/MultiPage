46e41bb9c3e962721a7c00f7dee12b075ea8bf57
Jimbos <tr9l9l9@hotmail.com>
2023-02-18 23:20:51 +0200

    Add project files.

---
+++ StockTracker/Shared/API Classes/Area.cs
@@ -0,0 +1,73 @@
+﻿using System.Text.Json.Serialization;
+
+namespace StockTracker.Shared.API_Classes {
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+    public class RootArea {
+        [JsonPropertyName("areas")]
+        public List<Area> areas { get; set; }
+
+        [JsonPropertyName("game_indices")]
+        public List<GameIndex> game_indices { get; set; }
+
+        [JsonPropertyName("id")]
+        public int id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string name { get; set; }
+
+        [JsonPropertyName("names")]
+        public List<Name> names { get; set; }
+
+        [JsonPropertyName("region")]
+        public Region region { get; set; }
+    }
+
+    public class Area {
+        [JsonPropertyName("name")]
+        public string name { get; set; }
+
+        [JsonPropertyName("url")]
+        public string url { get; set; }
+    }
+
+    public class GameIndex {
+        [JsonPropertyName("game_index")]
+        public int game_index { get; set; }
+
+        [JsonPropertyName("generation")]
+        public Generation generation { get; set; }
+    }
+
+    public class Generation {
+        [JsonPropertyName("name")]
+        public string name { get; set; }
+
+        [JsonPropertyName("url")]
+        public string url { get; set; }
+    }
+
+    public class Language {
+        [JsonPropertyName("name")]
+        public string name { get; set; }
+
+        [JsonPropertyName("url")]
+        public string url { get; set; }
+    }
+
+    public class Name {
+        [JsonPropertyName("language")]
+        public Language language { get; set; }
+
+        [JsonPropertyName("name")]
+        public string name { get; set; }
+    }
+
+    public class Region {
+        [JsonPropertyName("name")]
+        public string name { get; set; }
+
+        [JsonPropertyName("url")]
+        public string url { get; set; }
+    }
+
+}
